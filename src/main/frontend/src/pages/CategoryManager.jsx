ca909af40fbe5600e6e98b9d4e52e21f04dfedf6
Lukas Petr <lukas.petr.dev@gmail.com>
2024-03-21 10:25:25 +0100

    Frontend: add react-router
    This enables as to divide application to pages:
    - it will still be SPA,
    - we can have different URL for different pages,
    - it enables us to use back button.
    Category manager is now availabe at /category-manager.

---
+++ src/main/frontend/src/pages/CategoryManager.jsx
@@ -0,0 +1,57 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function CategoryManager() {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  function getCategories() {
+    // Fetches list of categories from REST API.
+    axios.get('/api/category')
+    .then(response => {
+        // todo handling of errors
+        setCategories(response.data);
+    })
+    .catch(error => console.log(error));
+  }
+  function addCategory(e) {
+    // Calls REST API for adding category.
+    e.preventDefault();
+    axios.post('/api/category', {name, description})
+      .then(response => {
+          if (response.status === 200) {
+          setDescription("");
+          setName("");
+          getCategories();
+          }
+      })
+      .catch(error => console.log(error));
+  }
+
+  // On load of page fetch list of categories.
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      <div>
+      {
+        categories.map(category => (<div key={category.id}>{category.name}: {category.description}</div>))
+      }
+      </div>
+      <h2>Add new category</h2>
+      <form onSubmit={addCategory}>
+        <label>Category name: </label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+        <br/>
+        <label>Description: </label>
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}></input>
+        <br/>
+        <input type="submit" value="Add category"/>
+      </form>
+    </div>
+  );
+}
