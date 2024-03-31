02aad5c44700db0452268aa20077a955833e1748
Lukas Petr <lukas.petr.dev@gmail.com>
2024-03-31 09:09:26 +0200

    Frontend: login updates
    - using renewToken API, tries to renew JWT token
      10 min before it expires,
    - saving role from login api

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -12,6 +12,9 @@ import {useContext, useEffect, useState} from "react";
 import {jwtDecode} from "jwt-decode";
 import {Modal} from "react-bootstrap";
 
+/** Time remaining for JWT expiration in which we should try to renew the JWT token. */
+const RENEW_AT_REMAINING_TIME = 600000; // (10 min)
+
 export default function App() {
   return (
     <AuthProvider>
@@ -36,17 +39,22 @@ export default function App() {
 function AuthVerify() {
   const [showLoggedOut, setShowLoggedOut] = useState(false);
   const location = useLocation();
-  const {user, logout} = useContext(AuthContext);
+  const {user, logout, renewToken} = useContext(AuthContext);
   useEffect(() => {
     if (user) {
       // user is logged in
       const {exp} = jwtDecode(user.token);
-      if (exp * 1000 < Date.now()) {
+      const remainingTime = exp * 1000 - Date.now();
+      if (remainingTime < 0) {
+        // Token expired
         logout();
         setShowLoggedOut(true);
+      } else if (remainingTime < RENEW_AT_REMAINING_TIME) {
+        // No much time left before JWT expires, try to renew it.
+        renewToken();
       }
     }
-  }, [location, user, logout]);
+  }, [location, user, logout, renewToken]);
   return (
       <Modal show={showLoggedOut} onHide={() => setShowLoggedOut(false)}>
         <Modal.Header closeButton>
