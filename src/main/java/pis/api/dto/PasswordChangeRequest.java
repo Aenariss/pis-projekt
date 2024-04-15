f2d27b282afb8536f4caa56b343c8022d009a600
Aenariss <aenariss@gmail.com>
2024-04-15 20:48:53 +0200

    when changing password, the old password is now required

--- src/main/java/pis/api/dto/PasswordChangeRequest.java
+++ src/main/java/pis/api/dto/PasswordChangeRequest.java
@@ -9,9 +9,11 @@ package pis.api.dto;
 public class PasswordChangeRequest extends Request {
     
     private String password;
+    private String old_password;
 
-    public PasswordChangeRequest(String password) {
+    public PasswordChangeRequest(String password, String old_password) {
         this.password = password;
+        this.old_password = old_password;
     }
 
     public PasswordChangeRequest() {}
@@ -23,5 +25,12 @@ public class PasswordChangeRequest extends Request {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public String getOld_password() {
+        return old_password;
+    }
+
+    public void setOld_password(String old_password) {
+        this.old_password = old_password;
+    }
 }
