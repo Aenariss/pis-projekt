84a36e07661e9ea1e1c00efaec9c3871bd08b553
Lukas Petr <lukas.petr.dev@gmail.com>
2024-04-17 18:51:46 +0200

    Frontend: requier original password for changing

--- src/main/frontend/src/components/UserForm/ChangePassword.jsx
+++ src/main/frontend/src/components/UserForm/ChangePassword.jsx
@@ -3,7 +3,7 @@
  * @author Lukas Petr.
  */
 import { useContext, useState } from 'react';
-import {Button, Form, Modal} from 'react-bootstrap';
+import {Alert, Button, Col, Form, Modal} from 'react-bootstrap';
 import Password from './Password';
 import { api } from '../../api';
 import { MessageContext } from '../../context/MessageContext';
@@ -15,19 +15,21 @@ import { MessageContext } from '../../context/MessageContext';
  */
 export default function ChangePassword({userId}) {
   const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
   const {setMessage} = useContext(MessageContext);
 
-  function handleSubmit(password) {
-    setShowModal(false);
+  function handleSubmit(old_password, password) {
     const url = userId
-      ? `/user/password/${userId}`
-      : `/user/password`;
-    api.put(url, {password})
-      .then(() => {
-        setMessage({text: 'Password has been successfully changed', variant: 'success'});
-      })
-      .catch(() => {
-        setMessage({text: 'Error: password could not be changed!', variant: 'danger'});
+    ? `/user/password/${userId}`
+    : `/user/password`;
+    api.put(url, {password, old_password})
+    .then(() => {
+      setShowModal(false);
+      setError(null);
+      setMessage({text: 'Password has been successfully changed', variant: 'success'});
+    })
+    .catch((error) => {
+        setError({text: `Error: ${error.response.data}`, variant: 'danger'});
       })
   }
   return (
@@ -39,9 +41,11 @@ export default function ChangePassword({userId}) {
         Change password
       </Button>
       { showModal && (
-        <ChangePasswordModal show={showModal}
+        <ChangePasswordModal requireOriginalPassword={userId === undefined}
+                             show={showModal}
                              onClose={() => setShowModal(false)}
-                             onSubmit={handleSubmit}/>
+                             onSubmit={handleSubmit}
+                             error={error}/>
         )
       }
     </>
@@ -52,14 +56,19 @@ export default function ChangePassword({userId}) {
  * Modal window for changing password.
  * @param {boolean} props.show If the modal should be shown.
  * @param {Function} props.onClose User want to close the modal.
- * @param {Function} props.onSubmit User send the form, called with password..
+ * @param {Function} props.onSubmit User send the form, called with password.
+ * @param {Function} props.requireOriginalPassword True to show field for filling the original password.
+ * @param {Function} props.error Error message to show to the user.
  * @returns 
  */
 function ChangePasswordModal({
   show,
   onClose,
   onSubmit,
+  requireOriginalPassword,
+  error,
 }) {
+  const [originalPassword, setOriginalPassword] = useState('');
   const [password1, setPassword1] = useState('');
   const [password2, setPassword2] = useState('');
   const [invalidPasswords, setInvalidPasswords] = useState();
@@ -67,7 +76,7 @@ function ChangePasswordModal({
   function handleSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
-    onSubmit(password1);
+    onSubmit(originalPassword, password1);
   }
 
   return (
@@ -77,6 +86,18 @@ function ChangePasswordModal({
       </Modal.Header>
       <Form onSubmit={handleSubmit}>
         <Modal.Body>
+          {error && <Alert variant={error.variant}>{error.text}</Alert>}
+          { requireOriginalPassword && (
+            <Form.Group controlId="password-original" as={Col}>
+              <Form.Label>Original password</Form.Label>
+              <Form.Control type="password"
+                            required
+                            value={originalPassword}
+                            onChange={(e) => setOriginalPassword(e.target.value)}
+                            isInvalid={originalPassword === ''} />
+              <Form.Control.Feedback type="invalid">Fill your original password</Form.Control.Feedback>
+            </Form.Group>
+          )}
           <Password password1={password1}
                     password2={password2}
                     onPassword1Change={setPassword1}
@@ -87,7 +108,7 @@ function ChangePasswordModal({
         </Modal.Body>
         <Modal.Footer>
           <Button type='submit'
-                  disabled={invalidPasswords}>
+                  disabled={invalidPasswords && (requireOriginalPassword && originalPassword === '')}>
             Change the password
           </Button>
         </Modal.Footer>
