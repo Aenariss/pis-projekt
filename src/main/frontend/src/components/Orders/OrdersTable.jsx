df43b1a3a0196c13b77f8994669a8ef9d54dab78
Lukas Petr <lukas.petr.dev@gmail.com>
2024-04-22 15:39:21 +0200

    Frontend: add pagination of orders

--- src/main/frontend/src/components/Orders/OrdersTable.jsx
+++ src/main/frontend/src/components/Orders/OrdersTable.jsx
@@ -5,6 +5,10 @@
 import { Button, Table } from 'react-bootstrap';
 import { orderStateToString } from '../../utils/orders';
 import { useNavigate } from 'react-router-dom';
+import Paginator from '../Paginator';
+import { useState } from 'react';
+
+const AMOUNT_OF_ORDERS_ON_PAGE = 15;
 
 /**
  * Table component for showing orders.
@@ -16,44 +20,53 @@ export default function OrdersTable({
   orders,
   type='user'
 }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const amountOfPages = Math.ceil(orders.length / AMOUNT_OF_ORDERS_ON_PAGE);
+  const ordersOnPage = orders.slice((currentPage-1) * AMOUNT_OF_ORDERS_ON_PAGE,
+                                        currentPage * AMOUNT_OF_ORDERS_ON_PAGE);
   const navigate = useNavigate();
   return (
-    <Table striped bordered>
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>Date of order</th>
-          {type === 'employee' && (
-            <th>User email</th>
-          )}
-          <th>State</th>
-          {type === 'user' && (
-            <th>Total price</th>
-          )}
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((order) => (
-          <tr key={order.id}>
-            <td>{order.id.toString().padStart(6,'0')}</td>
-            <td>{new Date(order.creationDate).toLocaleString()}</td>
+    <>
+      <Table striped bordered>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Date of order</th>
             {type === 'employee' && (
-              <td>{order?.email}</td>
+              <th>User email</th>
             )}
-            <td>{orderStateToString(order.status)}</td>
+            <th>State</th>
             {type === 'user' && (
-              <td>{order.totalPrice.toFixed(2)} $</td>
+              <th>Total price</th>
             )}
-            <td>
-              <Button size='sm'
-                      onClick={() => navigate(`/order/${order.id}`)}>
-                Detail
-              </Button>
-            </td>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {ordersOnPage.map((order) => (
+            <tr key={order.id}>
+              <td>{order.id.toString().padStart(6,'0')}</td>
+              <td>{new Date(order.creationDate).toLocaleString()}</td>
+              {type === 'employee' && (
+                <td>{order?.email}</td>
+              )}
+              <td>{orderStateToString(order.status)}</td>
+              {type === 'user' && (
+                <td>{order.totalPrice.toFixed(2)} $</td>
+              )}
+              <td>
+                <Button size='sm'
+                        onClick={() => navigate(`/order/${order.id}`)}>
+                  Detail
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Paginator pages={amountOfPages}
+                 activePage={currentPage}
+                 setActivePage={setCurrentPage}/>
+    </>
   );
 } 
