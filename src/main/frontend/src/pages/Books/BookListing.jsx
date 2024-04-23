d98f5e1e7b55caee954dff51884dc551d479208f
Lukas Petr <lukas.petr.dev@gmail.com>
2024-04-23 19:13:56 +0200

    Frontend books: showing no books were found

--- src/main/frontend/src/pages/Books/BookListing.jsx
+++ src/main/frontend/src/pages/Books/BookListing.jsx
@@ -21,26 +21,38 @@ export default function BookListing({products}) {
   const productsOnPage = products.slice((currentPage-1) * AMOUNT_OF_BOOKS_ON_PAGE,
                                         currentPage * AMOUNT_OF_BOOKS_ON_PAGE);
 
+  let content = (
+    <div className='display-5 text-center text-muted mt-5'>
+      No books were found
+    </div>
+  );
+  if (products.length > 0) {
+    content = (
+      <>
+        <Stack className="h-100">
+          <Row md={5}>
+            {productsOnPage.map(book =>
+              <Col key={book.id} className="p-2">
+                <Book id={book.id}
+                      name={book.name}
+                      author={book.author}
+                      price={book.price}
+                      image={book.image}
+                      discount={book?.discount?.discount}
+                      availableQuantity={book?.availableQuantity}/>
+              </Col>
+            )}
+          </Row>
+        </Stack>
+        <Paginator activePage={currentPage}
+                   pages={amountOfPages}
+                   setActivePage={setCurrentPage}/>
+      </>
+    );
+  }
   return (
     <Container className="h-100">
-      <Stack className="h-100">
-        <Row md={5}>
-          {productsOnPage.map(book =>
-            <Col key={book.id} className="p-2">
-              <Book id={book.id}
-                    name={book.name}
-                    author={book.author}
-                    price={book.price}
-                    image={book.image}
-                    discount={book?.discount?.discount}
-                    availableQuantity={book?.availableQuantity}/>
-            </Col>
-          )}
-        </Row>
-      </Stack>
-      <Paginator activePage={currentPage}
-                pages={amountOfPages}
-                setActivePage={setCurrentPage}/>
+      {content}
     </Container>
   );
 }
